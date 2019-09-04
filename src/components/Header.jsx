f11fc54feb9a955a94c6a79a09bd95f230ded0b8
smooth97 <dbtmdwls0506@naver.com>
2019-09-04 15:33:11 +0900

    ui

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+            <header>
+                <div className="headerContainer">
+                    Header
+                </div>
+            </header>
+    )
+}
+
+export default Header;
