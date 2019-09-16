5b8120e1a32cc84d8d3bbc27d5afc822ddd3193c
smooth97 <dbtmdwls0506@naver.com>
2019-09-16 15:31:24 +0900

    header contents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FiPlay } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
+import { FiInfo } from 'react-icons/fi';
 import backgroundImg from '../static/images/header.jpg';
 
 const Header = () => {
@@ -9,6 +12,15 @@ const Header = () => {
             <header>
                 <div className="headerContainer">
                     <img src={backgroundImg} alt="stranger"/>
+                    <div className="headerContents">
+                        <h1>STRANGER THINGS</h1>
+                        <p>When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.</p>
+                        <div className="btn">
+                            <button><FiPlay/><span>PLAY</span></button>
+                            <button><FiPlus/><span>MY LIST</span></button>
+                            <button><FiInfo/><span>DETAILS</span></button>
+                        </div>
+                    </div>
                 </div>
             </header>
     )
